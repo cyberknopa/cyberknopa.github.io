d598ba38a27da85baf2a73889f744dc93e558cfd
Veronica Gavrilova <81513344+cyberknopa@users.noreply.github.com>
2022-07-19 20:38:29 +0300

    Add files via upload

---
+++ jsc.js
@@ -0,0 +1,23 @@
+jQuery(($) => {
+  const speed = 1000;// скорость прокрутки в миллисек
+
+  $('a[href*="#"]')
+    .filter(
+      (i, a) =>
+        a.getAttribute("href").startsWith("#") ||
+        a.href.startsWith(`${location.href}#`)
+    )
+    .unbind("click.smoothScroll")
+    .bind("click.smoothScroll", (event) => {
+      const targetId = event.currentTarget.getAttribute("href").split("#")[1];
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        event.preventDefault();
+        $("html, body").animate(
+          { scrollTop: $(targetElement).offset().top },
+          speed
+        );
+      }
+    });
+});
